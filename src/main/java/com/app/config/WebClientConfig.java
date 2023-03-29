769bead0721c82ba9a43f26655af1c039e5dad58
Ashutosh Kumar Shuk;la <ashutosh.swd@gmail.com>
2023-03-29 13:36:27 +0530

    add the web client config

---
+++ src/main/java/com/app/config/WebClientConfig.java
@@ -0,0 +1,23 @@
+package com.app.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+@Slf4j
+public class WebClientConfig {
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+                .exchangeStrategies(ExchangeStrategies.builder()
+                        .codecs(configurer -> configurer
+                                .defaultCodecs()
+                                .maxInMemorySize(30 * 1024 * 1024))
+                        .build())
+                .build();
+    }
+}
