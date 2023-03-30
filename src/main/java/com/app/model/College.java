a69b5bec1f5497107ede184d02bed5b4a3fb39d7
Ashutosh Kumar Shuk;la <ashutosh.swd@gmail.com>
2023-03-30 16:03:07 +0530

    add the lombok in college class

--- src/main/java/com/app/model/College.java
+++ src/main/java/com/app/model/College.java
@@ -1,34 +1,19 @@
 package com.app.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class College {
 	
 	private int id;
 	private String collegeName;
 	private String collegeAddress;
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getCollegeName() {
-		return collegeName;
-	}
-	public void setCollegeName(String collegeName) {
-		this.collegeName = collegeName;
-	}
-	public String getCollegeAddress() {
-		return collegeAddress;
-	}
-	public void setCollegeAddress(String collegeAddress) {
-		this.collegeAddress = collegeAddress;
-	}
-	@Override
-	public String toString() {
-		return "College [id=" + id + ", collegeName=" + collegeName + ", collegeAddress=" + collegeAddress + "]";
-	}
+
 
 	
 }
